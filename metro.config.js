d1aa56e906ec6449f9d4d72d52f70d1a6174fa45
MuneebQuresihi1 <muneebmuhammad444@gmail.com>
2026-08-28 17:26:03 +0500

    fix: some metro issues

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
@@ -6,6 +7,22 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    resolveRequest(context, moduleName, platform) {
+      if (moduleName === 'zustand') {
+        return {
+          filePath: path.resolve(
+            __dirname,
+            'node_modules/zustand/index.js',
+          ),
+          type: 'sourceFile',
+        };
+      }
+
+      return context.resolveRequest(context, moduleName, platform);
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
